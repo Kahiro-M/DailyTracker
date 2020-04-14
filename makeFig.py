bec0fba7fa0b552903f7e45e481e476a8cea3646
Kahiro-M <0204.kahiro@gmail.com>
2020-04-14 09:54:53 +0900

    データ追加して棒グラフなどを追加

--- makeFig.py
+++ makeFig.py
@@ -42,25 +42,25 @@ csvData.to_json("./data.json")
 # 飲んだ量を翌日の日付と紐づけるために一日ずらしたデータに整形する。
 anlyDf=pd.DataFrame({
   "day":np.concatenate([
-      csvData.day,
-      csvData.day,
+      csvData.day[92:],
+      csvData.day[92:],
       [0], # 飲み物の日付が一日後ろにずれるので行列の大きさを調整する
     ]),
   "type":np.concatenate([
-      np.tile("Sleep(hour)",len(csvData.hour)),
-      (np.tile("Tea/Coffe(100ml)",len(csvData.drank))),
+      np.tile("Sleep(hour)",len(csvData.hour[92:])),
+      (np.tile("Tea/Coffe(100ml)",len(csvData.drank[92:]))),
       ["Tea/Coffe(100ml)"], # 飲み物の日付が一日後ろにずれるので行列の大きさを調整する
     ]),
   "data":np.concatenate([
-      csvData.hour,
+      csvData.hour[92:],
       [0], # この0mlを挿入することで飲み物の日付を翌日の睡眠時間と合わせる
-      csvData.drank/100,# 睡眠時間と有効桁数を合わせるために100ml単位にする
+      csvData.drank[92:]/100,# 睡眠時間と有効桁数を合わせるために100ml単位にする
     ]),
   })  
 print(anlyDf)
 
 # 折れ線グラフプロット
-fig = sns.lineplot(x="day", y="data",data=anlyDf,hue="type",style="type",markers=True)
+fig = sns.lineplot(x="day", y="data",data=anlyDf,hue="type",style="type")
 
 # x軸ラベルを設定＆ラベル表示を90度回転
 fig.set_xticklabels(anlyDf.day, rotation="90")
@@ -73,6 +73,18 @@ plt.title("Sleep time (hour) with drank Tea or Caffee (100ml) intake the day bef
 plt.savefig("line.png")
 plt.clf()
 
+
+# 曜日ごとの睡眠時間グラフプロット
+fig = sns.barplot(x="WoD",y="hour",data=csvData,color="0.9",order=['Sun.','Mon.','Tue.','Wed.','Thu.','Fri.','Sat.'])
+fig = sns.swarmplot(x="WoD",y="hour",data=csvData,order=['Sun.','Mon.','Tue.','Wed.','Thu.','Fri.','Sat.'])
+# 画像の保存と画像データのリフレッシュ
+plt.savefig("bar.png")
+plt.clf()
+
+fig = sns.boxplot(x="WoD",y="hour",data=csvData,color="0.95",order=['Sun.','Mon.','Tue.','Wed.','Thu.','Fri.','Sat.'])
+fig = sns.swarmplot(x="WoD",y="hour",data=csvData,order=['Sun.','Mon.','Tue.','Wed.','Thu.','Fri.','Sat.'])
+plt.savefig("box.png")
+plt.clf()
 
 # 散布図と回帰分析(信頼区間95%)プロット
 fig = sns.regplot(x="drank", y="hour", data=csvData, ci=95)
