b6691fa6be787e2ca1e2c1f546287dc32bde09ed
Kahiro-M <0204.kahiro@gmail.com>
2020-04-21 08:44:22 +0900

    2020.04.21

--- makeFig.py
+++ makeFig.py
@@ -12,7 +12,7 @@ html_header = """
 <html lang="ja">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <script src="./data.js"></script>
       <!--
@@ -36,7 +36,7 @@ csvData.to_json("./data.json")
 fig = sns.lineplot(x=csvData.day[153:],y=csvData.hour[153:],label="sleep[hour]")
 fig = sns.lineplot(x=csvData.day[153:],y=csvData.coffee[153:]/100,label="Tea/Coffee[100ml]")
 
-fig.set_xticklabels(csvData.day[153:], rotation="90")
+fig.set_xticklabels(csvData.day[153:],rotation="90")
 
 # 凡例、タイトル
 plt.legend()
@@ -60,7 +60,7 @@ plt.savefig("box.png")
 plt.clf()
 
 # 散布図と回帰分析(信頼区間95%)プロット
-fig = sns.regplot(x="coffee", y="hour", data=csvData[153:], ci=95)
+fig = sns.regplot(x="coffee",y="hour",data=csvData[153:],ci=95)
 # 画像の保存と画像データのリフレッシュ
 plt.savefig("reg.png")
 plt.clf()
@@ -74,7 +74,7 @@ plt.clf()
 
 
 # html output
-with open("index.html", mode="w", encoding="utf_8") as fileObj:
+with open("index.html",mode="w",encoding="utf_8") as fileObj:
   fileObj.write(html_header)
   fileObj.write("その日の睡眠時間と<b>前日の</b>カフェイン飲料摂取量<br>")
   fileObj.write("<img src='line.png'>")
